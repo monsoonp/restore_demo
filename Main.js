ef9e1e05e7e644f2bb721d962427749e39e8f66c
alstn0706 <alstn0706@naver.com>
2019-11-04 11:31:07 +0900

    first commit

---
+++ Main.js
@@ -0,0 +1,102 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  // LearnMoreLinks,
+  Colors,
+  // DebugInstructions,
+  // ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
+
+// const App: () => React$Node = () => {
+const App = () => {
+  return (
+    <ReactNativeZoomableView
+      zoomEnabled={true}
+      maxZoom={3}
+      minZoom={0.2}
+      initialZoom={1}
+      bindToBorders={true}>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <Text>can i get this?</Text>
+          </View>
+          {/*
+          <View style={styles.body}>
+            <LearnMoreLinks />
+          </View>
+          */}
+        </ScrollView>
+      </SafeAreaView>
+    </ReactNativeZoomableView>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
